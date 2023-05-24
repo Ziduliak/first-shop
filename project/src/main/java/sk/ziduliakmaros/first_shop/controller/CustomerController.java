a8c4da264ace62b1e8b7700ac7aecbac4ba53025
Maros <ziduliak.maros@gmail.com>
2023-05-24 19:40:34 +0200

    added account in CustomerController.java

--- project/src/main/java/sk/ziduliakmaros/first_shop/controller/CustomerController.java
+++ project/src/main/java/sk/ziduliakmaros/first_shop/controller/CustomerController.java
@@ -4,10 +4,15 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
+import sk.ziduliakmaros.first_shop.model.dto.CustomerAccountDto;
 import sk.ziduliakmaros.first_shop.model.dto.CustomerDto;
 import sk.ziduliakmaros.first_shop.model.dto.controller.customer.AddCustomerRequest;
 import sk.ziduliakmaros.first_shop.model.dto.controller.customer.AddCustomerResponse;
+import sk.ziduliakmaros.first_shop.model.dto.controller.customerAccount.CustomerAccountRequest;
+import sk.ziduliakmaros.first_shop.model.dto.controller.customerAccount.CustomerAccountResponse;
+import sk.ziduliakmaros.first_shop.service.CustomerAccountService;
 import sk.ziduliakmaros.first_shop.service.CustomerService;
+
 
 import java.util.List;
 
@@ -17,7 +22,8 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
-
+    @Autowired
+    private CustomerAccountService customerAccountService;
 
 
     @PostMapping(path = "/addCustomer")
@@ -53,6 +59,18 @@ public class CustomerController {
         customerService.deleteCustomer(id);
     }
 
-
+    @PostMapping("/account")
+    public CustomerAccountResponse addAccount(@RequestBody @Valid CustomerAccountRequest account) {
+        final CustomerAccountResponse response = new CustomerAccountResponse();
+        response.setAccount(customerAccountService.createAccount(
+                account.getCustomerId(),
+                account.getMoney()));
+        return response;
+    }
+    @GetMapping(path = "/allAccount")
+    public List<CustomerAccountDto> getAllAccounts() {
+        // This returns a JSON or XML with the users
+        return customerAccountService.getAllCustomers();
+    }
 }
 
